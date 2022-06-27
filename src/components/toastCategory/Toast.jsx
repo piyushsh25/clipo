31f2f4a187b0bb8a51af551defde1810875ebb36
piyush <piyushsharmashpr@gmail.com>
2022-06-27 23:45:14 +0530

    refactor+feat toast for filter in home page

--- src/components/toastCategory/Toast.jsx
+++ src/components/toastCategory/Toast.jsx
@@ -3,12 +3,14 @@ import { useCategory } from "../../hooks/useCategory"
 import { useFilterContext } from "../../hooks/CategoryFilter/FilterContext";
 export function Toast() {
     const { category, setCategory } = useCategory();
-    const {FilteredVideo, state, dispatch}= useFilterContext();
+    const { FilteredVideo, state, dispatch } = useFilterContext();
     return <div className="homepage-filter">
+        <div onClick={() => dispatch({ type: null })} className="clear-filter">
+            All
+        </div>
         {category.map(({ categoryName, description, id }) => {
-            console.log(categoryName)
-            return <div>   
-               <input type="radio" id={`radio${id}`} name="radios" value="all" onChange={()=>dispatch({type:categoryName})}/>
+            return <div>
+                <input type="radio" id={`radio${id}`} name="radios" value="all" onChange={() => dispatch({ type: categoryName })} checked={state.categoryName===categoryName}/>
                 <label for={`radio${id}`}>{categoryName}</label>
             </div>
         })}

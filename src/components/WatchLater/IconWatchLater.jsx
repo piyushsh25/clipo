368bc6af7c1d6588d2542ae038d14a88618c6c17
piyush <piyushsharmashpr@gmail.com>
2022-08-05 19:44:15 +0530

    [feat] login modal on like(if not logged in)

--- src/components/WatchLater/IconWatchLater.jsx
+++ src/components/WatchLater/IconWatchLater.jsx
@@ -1,10 +1,12 @@
 import { useWatchLater } from "../../hooks/WatchLater/WatchContext"
 import "./LaterHeader.css"
+import {useAuth} from "../../hooks/Auth/AuthContext"
 export const IconWatchLater = ({ video }) => {
     const { watchLaterArray, removeFromLater, addToLater } = useWatchLater()
+    const { setShowLoginModal } = useAuth()
     return <>
         {watchLaterArray.some((videos) => {
             return videos._id === video._id
-        }) ? <i className="fas fa-heart fill" onClick={() => removeFromLater(video)} /> : <i className="fas fa-heart" onClick={() => addToLater(video)}></i>}
+        }) ? <i className="fas fa-heart fill" onClick={() => removeFromLater(video)} /> : <i className="fas fa-heart" onClick={() =>localStorage.getItem("clipoToken") ? addToLater(video)(true) : setShowLoginModal(true)}></i>}
     </>
 }
